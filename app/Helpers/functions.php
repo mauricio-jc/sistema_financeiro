315e0c59db249beb3664c48111ba90b76e25bcc6
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2021-01-12 11:24:14 -0300

    Refazer

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -19,26 +19,6 @@
 		return $newDate;
 	}
 
-	function sanitizePhone($phone) {
-        $phone = str_replace('(', '', $phone);
-        $phone = str_replace(')', '', $phone);
-        $phone = str_replace(' ', '', $phone);
-        $phone = str_replace('-', '', $phone);
-        return $phone;
-	}
-
-    function sanitizeCpf($cpf) {
-        $cpf = str_replace('.', '', $cpf);
-        $cpf = str_replace('-', '', $cpf);
-        return $cpf;
-    }
-
-    function sanitizeCnpj($cnpj) {
-        $cnpj = str_replace('.', '', $cnpj);
-        $cpf = str_replace('/', '', $cnpj);
-        return $cnpj;
-    }
-
     function convertDecimalDb($price) {
         $price = str_replace('.', '', $price);
         $price = str_replace(',', '.', $price);
@@ -47,14 +27,6 @@
 
     function formatCoin($value) {
         return number_format($value, 2, ',', '.');
-    }
-
-    function convertCents($valor) {
-        return str_replace('.', '', $valor);
-    }
-
-    function formatWeight($value) {
-        return number_format($value, 3, ',', '.');
     }
 
     function verifyEmail($email) {
@@ -74,101 +46,4 @@
             }
         }
     }
-
-    function getExplodeIdClient($client) {
-        $arrClient = explode(' - ', $client);
-        return $arrClient[0];
-    }
-
-    function translateStatus($status) {
-        switch ($status) {
-            case 'processing':
-                return 'Processando';
-                break;
-            case 'authorized':
-                return 'Autorizado';
-                break;
-            case 'paid':
-                return 'Pago';
-                break;
-            case 'waiting_payment':
-                return 'Aguardando pagamento';
-                break;
-            case 'analyzing':
-                return 'Analisando';
-                break;
-            case 'pending_refund':
-                return 'Reembolso pendente';
-                break;
-            case 'pending_refund':
-                return 'Cobrado de volta';
-                break;
-            case 'pending_review':
-                return 'Revisão pendente';
-                break;
-            case 'expired':
-                return 'Vencido';
-                break;
-            default:
-                return '';
-                break;
-        }
-    }
-
-    function translatePayment($payment, $boletoUrl = null) {
-        if($payment == 'dinheiro') {
-            return 'Dinheiro';
-        }
-
-        if($payment == 'credit_card') {
-            return 'Cartão de crédito';
-        }
-        
-        if($payment == 'boleto') {
-            echo "<a href='$boletoUrl' target='_blank'>Imprimir boleto</a>";
-        }
-    }
-
-
-    function getParcelas($valor) {
-        if($valor <= 99) {
-            $parcelas = 1;
-        }
-        else if($valor > 99 && $valor <= 150) {
-            $parcelas = 2;
-        }
-        else if($valor > 150 && $valor <= 250) {
-            $parcelas = 3;
-        }
-        else if($valor > 250 && $valor <= 350) {
-            $parcelas = 4;
-        }
-        else if($valor > 350 && $valor <= 450) {
-            $parcelas = 5;
-        }
-        else if($valor > 450 && $valor <= 550) {
-            $parcelas = 6;
-        }
-        else if($valor > 550 && $valor <= 650) {
-            $parcelas = 7;
-        }
-        else if($valor > 650 && $valor <= 750) {
-            $parcelas = 8;
-        }
-        else if($valor > 750 && $valor <= 850) {
-            $parcelas = 9;
-        }
-        else if($valor > 850 && $valor <= 950) {
-            $parcelas = 10;
-        }
-        else if($valor > 950 && $valor <= 1199) {
-            $parcelas = 11;
-        }
-        else {
-            $parcelas = 12;
-        }
-
-        return $parcelas;
-    }
-
 ?>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
-
-Route::get('/contas-receber', 'ContasReceberController@index');
-Route::get('/contas-receber/adicionar', 'ContasReceberController@add');
-Route::post('/contas-receber/salvar', 'ContasReceberController@store');
-Route::get('/contas-receber/editar/{id}', 'ContasReceberController@edit');
-Route::post('/contas-receber/atualizar/{id}', 'ContasReceberController@update');
